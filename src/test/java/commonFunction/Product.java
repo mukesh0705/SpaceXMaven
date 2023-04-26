c59afb8426470d8a1bda90293c336b0357cf6cab
MUKESH <MUKESH@MUKESH-PC>
2023-04-26 20:31:09 +0530

    SpaceXshoping

---
+++ src/test/java/commonFunction/Product.java
@@ -0,0 +1,35 @@
+package commonFunction;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+import org.testng.Assert;
+public class Product  {
+	WebDriver driver;
+	public Product(WebDriver driver) {
+		this.driver=driver;
+
+	}
+	public static By itemPrice=By.xpath("//span[@class='money cash-money pre-money']");
+	public static By itemDescription=By.xpath("//strong[normalize-space()='Embroidered SpaceX logo']");
+	public static By addToCart=By.xpath("//span[normalize-space()='Add to cart']");
+	public static By quantity=By.name("quantity");
+	public static By cartQuantity=By.xpath("//span[@data-action='increase-quantity']");
+
+	public void verifyProductDetails(float price,String details) {
+
+		//Assert.assertEquals(driver.findElement(itemPrice).getText().replace("$", ""),price);
+		Assert.assertEquals(driver.findElement(itemDescription).getText(),details);
+	}
+
+	public void addQuantity(int i) {
+
+		for (int j=1;j<=i;j++) {
+
+			driver.findElement(cartQuantity).click();	
+		}
+		driver.findElement(addToCart).click();
+	}
+
+
+}
---
+++ src/test/java/driverFactory/TestDrive.java
@@ -0,0 +1,45 @@
+package driverFactory;
+
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+import static org.hamcrest.Matchers.equalTo;
+import commonFunction.CartPage;
+import commonFunction.Product;
+import commonFunction.SearchPage;
+import config.AppUtil;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+public class TestDrive extends AppUtil {
+
+	@Test(priority = 0,description = "Test Get API and Validate Content")
+	public void ProductVerification() {
+		SearchPage frontPage=PageFactory.initElements(driver, SearchPage.class);
+		frontPage.searchItems("spacex");
+		frontPage.verify_product();
+
+	}
+	@Test(priority = 1,description = "Shop SpaceX-Search, Add to Cart")
+	public void addQuantity() {
+
+		Product product=PageFactory.initElements(driver,Product.class);
+		product.verifyProductDetails(40f,"Embroidered SpaceX logo");
+		product.addQuantity(3);
+        RestAssured.given()
+        .contentType(ContentType.JSON)
+        .when()
+        .get("https://swapi.dev/api/people/")
+        .then()
+        .assertThat()
+        .body("results[2].name", equalTo("R2-D2"))
+        .and()
+        .body("results[2].skin_color", equalTo("white, blue"));
+	}
+	@Test(priority = 2,description = "Test Get API and Validate Cart Price")
+	public void cartVerification() {
+		CartPage cart=PageFactory.initElements(driver, CartPage.class);
+		cart.verifyPrice(4,40);
+	}
+}
+
+
